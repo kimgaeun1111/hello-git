636d2f3a088899402ca96db5b13d5c890fba3d44
kimgaeun1111 <sallyga1111@naver.com>
2024-12-03 03:29:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = http.createServer((req,res) => {
 });
 
 server.listen(port,hostname,() => {
-  console.log('Server is running at http://${hostname}:${port}/');
+   console.log(`Server is running at http://${hostname}:${port}/`); 
 });
 
 module.exports=server;
