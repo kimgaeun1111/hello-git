89dade7ccb90ebc7eb2f7208b3de93c1cfaf4461
kimgaeun1111 <sallyga1111@naver.com>
2025-03-06 14:25:29 +0900

    Title: 정수 선언하고 곱 출력, Time: 93ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250306/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
+++ 250306/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int x=26;
-        int y=5;
-        int z=x*y;
-        System.out.print(x+" * "+y+" = "+z);
+        // Please write your code here.
     }
 }
