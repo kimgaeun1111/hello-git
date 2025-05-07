67ebad545d75099b6bc52b4631fb577343497b40
kimgaeun1111 <sallyga1111@naver.com>
2025-05-08 01:44:50 +0900

    Title: 길이 단위 변환하기, Time: 158ms, Memory: 14MB, Status: Passed - Codetree

---
+++ 250508/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,7 @@
+let ft = 30.48;
+let mi = 160934;
+let resultFt = 9.2 * ft;
+let resultMi = 1.3 * mi;
+
+console.log(`9.2ft = ${resultFt.toFixed(1)}cm`);
+console.log(`1.3mi = ${resultMi.toFixed(1)}cm`);
